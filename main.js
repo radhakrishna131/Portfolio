09e2feb9e2d64d0b3a9c918f1c48fb297403ba89
radhakrishna131 <radhakrishna628191@gmail.com>
2025-07-05 22:45:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const m = document.querySelector(".m");
+const menu_bar = document.querySelector(".navbar-menu .menu-bar");
+m.addEventListener("click",()=>{menu_bar.classList.toggle("show")
+  
+})
